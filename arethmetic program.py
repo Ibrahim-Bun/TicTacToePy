942fa6c1df49566e657403dfd1963f1a28b70274
Ibrahim-Bun <IbrahimBundu500@gmail.com>
2025-09-10 20:36:25 +0100

    Add files via upload

---
+++ arethmetic program.py
@@ -0,0 +1,22 @@
+a = 3
+b = 2
+
+#sum of a and b
+sumof = a+b
+
+
+#difference of a and b
+if a>b:
+    diff = a-b
+else:
+    diff = b-a
+
+
+#product of a and b
+prodof = a*b
+
+print(sumof)
+print(diff)
+print(prodof)
+
+#To allow user input use int(input("Enter Number"))
---
+++ firstproject - Copy.py
@@ -0,0 +1,25 @@
+
+#grade boundary program
+
+
+#input
+
+
+
+points = int(input("Enter grade"))
+    
+if points < 21 and points < 21:
+ print("Fail")
+
+elif points >= 21 and points < 34:
+ print("Pass")
+
+elif points >= 34 and points < 48 :
+ print("Merit")
+
+if points >= 48:
+ print("Distinction")
+
+
+
+
